284b27eeb9a52428e58f5cfa6e96f18458b29127
Daphp <daphinenyangena99@gmail.com>
2024-10-24 16:45:10 +0300

    Added function to run on page load.

--- static/js/app.js
+++ static/js/app.js
@@ -103,20 +103,30 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
+    let names = data.names;
 
 
     // Use d3 to select the dropdown with id of `#selDataset`
+    let dropdown = d3.select("#selDataset");
 
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
+    names.forEach((name) => {
+      dropdown.append("option")
+        .text(name)
+        .property("value", name);
+    });
 
 
     // Get the first sample from the list
+    let firstSample = names[0];
 
 
     // Build charts and metadata panel with the first sample
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
 
   });
 }
